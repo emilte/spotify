4bf68e9e2a5d510ee7ced794353cb81510127816
Emil Telstad <22004178+emilte@users.noreply.github.com>
2023-11-22 15:20:09 +0100

    Update bpm.

--- bpm.py
+++ bpm.py
@@ -1,4 +1,5 @@
-from utils import Scope, scope_builder
+import os
+from utils import Scope
 from mytypes.types import AudioFeaturesObject, PlaylistObject
 from spotipy_client import SpotipyClient
 
@@ -35,7 +36,7 @@ def duplicate_playlist_sorted_by_bpm(
 
     plname = name or f'{playlist.name} - by bpm'
     if not dry_run:
-        new_pl: PlaylistObject = client.user_playlist_create(user=emil, name=plname)
+        new_pl: PlaylistObject = client.user_playlist_create(user=user, name=plname)
         client.playlist_add_items(playlist_id=new_pl.uri, items=sorted_audio_features_uris)
     print(f'Created playlist: {plname}')
 
@@ -43,13 +44,13 @@ def duplicate_playlist_sorted_by_bpm(
 if __name__ == '__main__':
     scopes = [Scope.playlist_modify_private, Scope.playlist_modify_public]
 
-    slow_wcs_playlist, slow_t = 'https://open.spotify.com/playlist/0w0YxOMjvKUFS215VPIK6X?si=af0f0880708744c8', 130
+    slow_wcs_playlist, slow_t = 'https://open.spotify.com/playlist/0w0YxOMjvKUFS215VPIK6X?si=af0f0880708744c8', 100
     inputpl = 'https://open.spotify.com/playlist/5ISmAZvdFQob5nEZgH9ufI?si=4ee7af478b0b4a38'
     test = 'https://open.spotify.com/playlist/0zTGrbhMKJcOnCnAn55vGZ?si=34295a468ad74e65'
     fast_wcs = 'https://open.spotify.com/playlist/5lTsNph8TgIrTR7znoGfGk?si=49125ed39fe7483e'
-    emil = 'emiltelstad'
+    user = os.environ['USER']
 
-    sp = SpotipyClient(username=emil, scope=scopes)
+    sp = SpotipyClient(username=user, scope=scopes)
 
     USING_PL = slow_wcs_playlist
     THRESHOLD = None
